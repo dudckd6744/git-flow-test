82dc72b0426fe73721d9207e4b6ecf98e4f33d91
dudckd6744 <dudckd6744@naver.com>
2022-06-12 15:29:18 +0900

    test1232

--- apps/order/src/app.service.ts
+++ apps/order/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello orde3ddf3r';
+    return 'Hello orde3d1df3r';
   }
 }
